3060890d62dd3b1588f510f079e0f409f5e7ac74
breno manoel <breno24.caixeta@gmail.com>
2026-01-21 15:53:11 -0300

    H2 database, test profile, JPA

--- src/main/java/com/PulseStore/PulseStore/entities/User.java
+++ src/main/java/com/PulseStore/PulseStore/entities/User.java
@@ -1,10 +1,18 @@
 package com.PulseStore.PulseStore.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.io.Serializable;
 
+@Entity
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
