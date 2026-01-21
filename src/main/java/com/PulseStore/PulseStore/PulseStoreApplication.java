0601daa75b2f0deec11347769c5484a6c2ffc720
breno manoel <breno24.caixeta@gmail.com>
2026-01-21 14:57:41 -0300

    Project created

---
+++ src/main/java/com/PulseStore/PulseStore/PulseStoreApplication.java
@@ -0,0 +1,13 @@
+package com.PulseStore.PulseStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseStoreApplication.class, args);
+	}
+
+}
